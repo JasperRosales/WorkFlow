a6ed93853b45f8a40e9055a3c3abecfe32fc4dfa
Asper The Ghost <23-19300@g.batstate-u.edu.ph>
2025-03-28 06:21:45 +0800

    Add files via upload
    sixth init

---
+++ workflow-user-service/src/main/java/com/ncnl/workflow_user_service/WorkflowUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ncnl.workflow_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowUserServiceApplication.class, args);
+	}
+
+}
---
+++ workflow-user-service/src/test/java/com/ncnl/workflow_user_service/WorkflowUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncnl.workflow_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
